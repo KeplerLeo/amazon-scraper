903518cfda344c5b059c53cfffee9f61366b91a5
Leonardo Kepler Mesquita <leokepler@gmail.com>
2025-04-30 15:09:11 -0300

    Update Docker for dev env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,13 @@ export default {
     server: {
         port: 4173,
         host: true,
-        strictPort: true
+        strictPort: true,
+        watch: {
+            usePolling: true,
+            interval: 1000
+        },
+        hmr: {
+            clientPort: 4173
+        }
     }
 }
